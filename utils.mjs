c5674ffc28e1ce38f360f19085fa1ff01379345e
belu20 <isanseptian20@gmail.com>
2024-10-07 21:13:19 +0700

    ubah file utils

--- utils.mjs
+++ utils.mjs
@@ -1,18 +1,48 @@
-function promiseExecutor(resolve, reject){
-    setTimeout (() => {
-        console.log('melakukan sesuatu sebelum promise selesai.')
-
+export function makeCoffee(name) {
+    return new Promise((resolve, reject) => {
+      const estimationTime = 2000;
+      let isSuccess = false;
+  
+      const inSecond = Math.ceil(estimationTime / 1000);
+      console.log(`Mohon menunggu. Pramusaji sedang membuatkan kopi dalam ${inSecond} detik`);
+  
+      setTimeout(() => {
         const number = Math.random();
-        if (number > 0.5){
-            resolve('you dit it');
-
+        if (number > 0.3) {
+          isSuccess = true;
         }
-        else {
-            reject('sorry, something wrong!');
+  
+        if (!isSuccess) {
+          reject(new Error('Maaf, kopi gagal dibuatkan.'));
+          return;
         }
-    }, 2000);
-} 
-
-export function doSomething (){
-    return new Promise(promiseExecutor);
-}
+  
+        console.log('Pramusaji selesai membuat kopi.');
+        resolve(name);
+      }, estimationTime);
+    });
+  }
+  
+  export function sendCoffee(name) {
+    return new Promise((resolve, reject) => {
+      const estimationTime = 1000;
+      let isSuccess = false;
+  
+      console.log('Pramusaji sedang mengantarkan kopi pesanan');
+  
+      setTimeout(() => {
+        const number = Math.random();
+        if (number > 0.1) {
+          isSuccess = true;
+        }
+  
+        if (!isSuccess) {
+          reject(new Error('Maaf, kopi gagal diantarkan.'));
+          return;
+        }
+  
+        console.log('Pramusaji sudah sampai ke meja.');
+        resolve(name);
+      }, estimationTime);
+    });
+  }
